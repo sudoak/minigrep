7c63112897861606d5dd5f584698eabb25b20de7
sudoak <sudoakk@gmail.com>
2020-04-29 22:47:04 +0530

    [refactor] : handled errors using unwrap_or_else, Result<T,Err>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
-use std::env;
-use std::fs;
+use std::{process, env};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    // parsing input
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem passing arguments {}",err);
+        process::exit(1);
+    });
+}
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename)
-        .expect("unable to read file");
-    
-    println!("With text:\n{}", contents);
+struct Config {
+    query: String,
+    filename: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
 }
