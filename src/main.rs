81493bc0f22b590327c7128894e95f97ed46163f
sudoak <sudoakk@gmail.com>
2020-04-29 22:18:48 +0530

    [feat] : read contents from file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use std::env;
+use std::fs;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("unable to read file");
+    
+    println!("With text:\n{}", contents);
 }
