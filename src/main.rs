1de0cc7b7524af649b6158fc596c3335183ff3cd
sudoak <sudoakk@gmail.com>
2020-04-30 19:29:06 +0530

    [feat] : working version of minigrep

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,12 @@ use minigrep::{Config,run};
 fn main() {
     let args: Vec<String> = env::args().collect();
     // parsing input
-    let config = Config::new(&args).unwrap_or_else(|err|{
-        println!("Problem passing arguments {}",err);
+    let config = Config::new(&args).unwrap_or_else(|_err|{
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+    if let Err(_e) = run(config) {
         process::exit(1);
     }
     
 }
-
-// struct Config {
-//     query: String,
-//     filename: String,
-// }
-// impl Config {
-//     fn new(args: &[String]) -> Result<Config, &'static str> {
-//         if args.len() < 3 {
-//             return Err("not enough arguments");
-//         }
-//         let query = args[1].clone();
-//         let filename = args[2].clone();
-//         Ok(Config { query, filename })
-//     }
-// }
-
-// fn run(config: Config) -> Result<(), Box<dyn Error>>{
-//     let contents = fs::read_to_string(config.filename)?;
-//         println!("With text:\n{}", contents);
-//         Ok(())
-// }
