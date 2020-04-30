6b6792893770fd1716e7f362c07bb15615784127
sudoak <sudoakk@gmail.com>
2020-04-30 19:39:05 +0530

    [refactor] : writing error to stderr

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,21 @@ use minigrep::{Config,run};
 fn main() {
     let args: Vec<String> = env::args().collect();
     // parsing input
-    let config = Config::new(&args).unwrap_or_else(|_err|{
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        eprintln!("Problem parsing arguments: {}", err); 
         process::exit(1);
     });
 
-    if let Err(_e) = run(config) {
+    if let Err(e) = run(config) {
+        eprintln!("Problem parsing arguments: {}", e); 
         process::exit(1);
     }
     
 }
+
+
+// let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+// can set env vars and import in our program
+
+// eprintln!("Problem parsing arguments: {}", err); 
+// can write 
